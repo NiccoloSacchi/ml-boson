bb369ca88cf6d5d95058112ed885f7094a718cd9
vnigolian <valentin.nigolian@gmail.com>
2017-10-15 18:30:22 +0200

    boson data is loaded

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -3,6 +3,30 @@
 
 import numpy as np
 
+#if sub_sample = true, we only load the first 100 lines of each set
+#for now only the first 100 lines version works
+def load_boson_data(sub_sample=True):
+    path_train_dataset = "train_100.csv"
+    path_test_dataset = "train_100.csv"
+    
+    ids_tr = np.genfromtxt(
+        path_train_dataset, delimiter=",", skip_header=1,usecols=0)
+        
+    predictions_tr = np.genfromtxt(
+        path_train_dataset,delimiter=",",skip_header=1,usecols=1,dtype=None,
+        converters={1: lambda x: 1 if b's' in x else -1})
+    
+    data_tr = np.genfromtxt(
+        path_train_dataset, delimiter=",", skip_header=1,usecols=range(2,32))
+    
+    ids_te = np.genfromtxt(
+        path_test_dataset, delimiter=",", skip_header=1,usecols=0)
+        
+    data_te = np.genfromtxt(
+        path_test_dataset, delimiter=",", skip_header=1,usecols=range(2,32))
+    
+    return ids_tr,predictions_tr,data_tr, ids_te, data_te
+
 def load_data(sub_sample=True, add_outlier=False):
     """Load data and convert it to the metric system."""
     path_dataset = "height_weight_genders.csv"
